6cd8c31c297ef4a438de9b6439d43bfdb826d40d
Jason Swetzoff <swetzoff@gmail.com>
2012-10-27 00:48:20 -0400

    Version 1.1: Reorganized structure.

--- config.rb
+++ config.rb
@@ -1,14 +1,12 @@
-require 'susy'
-require 'color-schemer'
-require 'sassy-buttons'
 # Require any additional compass plugins here.
+require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "code/css"
-sass_dir = "code/scss"
-images_dir = "assets/image-base"
-javascripts_dir = "code/js"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
